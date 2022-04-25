10cd2b683887ca7c4a47368a1f70a44696aafa00
samschool789@gmail.com <samschool789@gmail.com>
2022-04-24 22:14:40 -0500

    variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-var weatherApi = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9209536caeff33b08c2db4e1b75caa85";
-
+const weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=39.09&lon=94.57&exclude=currentminutelyhouryalerts&appid=9209536caeff33b08c2db4e1b75caa85";
+const inputEl = document.getElementById("input-button");
+const searchEl = document.getElementById("search-button");
 
 function getAPI() {
     fetch(weatherApi)
     .then(response => response.json())
     .then(data => console.log(data));
+
+    
 
 }
 getAPI()
