f2af6fcb91a045acdd7da401e7ea0f7faf9f1715
samschool789@gmail.com <samschool789@gmail.com>
2022-04-26 10:19:17 -0500

    api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-const weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=39.09&lon=94.57&exclude=currentminutelyhouryalerts&appid=9209536caeff33b08c2db4e1b75caa85";
-const inputEl = document.getElementById("input-button");
-const searchEl = document.getElementById("search-button");
-
+var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=39.09&lon=94.57&exclude=currentminutelyhouryalerts&appid=9209536caeff33b08c2db4e1b75caa85";
+var tempatureEl = document.querySelector("#input-button");
+var displayWeather = function(current, searchTerm) {
+    console.log(current);
+}
 function getAPI() {
     fetch(weatherApi)
     .then(response => response.json())
     .then(data => console.log(data));
 
-    
+
 
 }
 getAPI()
