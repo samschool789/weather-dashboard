5a7977c39ad017a90d3fb81b1515454949d736d7
samschool789@gmail.com <samschool789@gmail.com>
2022-04-30 21:24:14 -0500

    README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# weather-dashboard
+# WEATHER DASHBOARD
+
+# My job was to make a dashboard that would display a 5 day forecast when you select a city
+
+# It will display the city name, the date, an icon representation of weather conditions, temp,
+# humidity, the wind speed and UV index
+
+# Have the city you initally select save to use at a later time
+
+# Show the Future weather conditions for the city 
+
+
+[project link](https://github.com/samschool789/work-day-scheduler)
+
+![image](img23.png)
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
-var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=39.09&lon=94.57&exclude=currentminutelyhouryalerts&appid=9209536caeff33b08c2db4e1b75caa85";
-var tempatureEl = document.querySelector("#input-button");
-var displayWeather = function(current, searchTerm) {
-    console.log(current);
+var searchEl = document.querySelector("#search-input");
+var searchButton = document.querySelector("#search-button");
+var currentWeatherEl = document.querySelector("#current-weather");
+
+function getWeather(cityname) {
+  var geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=9209536caeff33b08c2db4e1b75caa85`;
+
+  fetch(geoApi)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      var weatherApi = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${lat}&lon=${lon}&exclude=currentminutelyhouryalerts&appid=9209536caeff33b08c2db4e1b75caa85`;
+      fetch(weatherApi)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          var temp = data.current.temp;
+          var tempEl = document.createElement("p");
+          tempEl.textContent = temp;
+          currentWeatherEl.append(tempEl);
+        });
+    });
 }
-function getAPI() {
-    fetch(weatherApi)
-    .then(response => response.json())
-    .then(data => console.log(data));
 
-
-
-}
-getAPI()
+searchButton.addEventListener("click", function () {
+  var cityname = searchEl.value;
+  getWeather(cityname);
+});
